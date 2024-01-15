db06bb752c3749a22ce39d3aab24d5da7ba88f4c
SeanQuinn <Seanquinn917@gmail.com>
2024-01-15 17:34:00 -0500

    Index in AptController/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   resources :reviews
   resources :tenants
   resources :apartments
+
+  get "/apartments", to: "apartments#index"
+
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
