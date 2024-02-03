10d32e0d43f53b263f4c7ec953c419bbe71f416d
SeanQuinn <Seanquinn917@gmail.com>
2024-02-03 16:01:20 -0500

    production.rb debug

--- app/serializers/tenant_serializer.rb
+++ app/serializers/tenant_serializer.rb
@@ -1,4 +1,6 @@
 class TenantSerializer < ActiveModel::Serializer
+
+  
   attributes :id, :name, :age, :username, :lease_id, :lease_content, :rent, :apartment_id, :avatar
   belongs_to :lease
 
@@ -15,5 +17,5 @@ class TenantSerializer < ActiveModel::Serializer
     object.lease&.apartment_id
   end
 
-
+  
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
 
   # Ensures that a master key has been made available in ENV["RAILS_MASTER_KEY"], config/master.key, or an environment
   # key such as config/credentials/production.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+  config.require_master_key = true
 
   # Disable serving static files from `public/`, relying on NGINX/Apache to do so instead.
    config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
@@ -32,6 +32,8 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
+  config.action_mailer.default_url_options = {host: "https://apartment-app-jfxo.onrender.com"}
+  
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
